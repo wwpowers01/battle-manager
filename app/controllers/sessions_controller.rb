f18b3a47b7ac8bc65e096017c1f866cf0371e8e7
Warren Powers <wwpowers01@gmail.com>
2018-12-16 12:11:11 -0500

    user creation and log in complete
    rudamentary user creation and logging in complete. log in works, 
    registration works. needs to be cleaned up and throughly tested though.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_url
+    else
+      flash[:danger] = "Login has failed. Invalid email or password."
+      redirect_to root_url
+    end
+  end
 end

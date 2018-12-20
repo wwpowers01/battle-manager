c375d022080dfc21d8c2b3e249f6a2b68d78dbb8
Warren Powers <wwpowers01@gmail.com>
2018-12-20 11:23:26 -0500

    new routes
    added routes in preperation of combatants

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
   post '/login',        to: 'sessions#create'
   delete '/logout',     to: 'sessions#destroy'
 
-  resources :combats, only: [:create, :show, :new] do
+  resources :combats, only: [:create, :show, :new, :index] do
     member do
       patch 'next'
       patch 'previous'
       patch 'roll'
+    end
+    collection do
+      patch 'damage'
+      patch 'heal'
     end
   end
 

151ef5e67eb75b2a1f6ecef3853650660f8646b9
Warren Powers <wwpowers01@gmail.com>
2018-12-21 14:25:54 -0500

    added selectable and sortable combatant list
    this list is now selectable and sortable. just an example of how it will 
    look. need items to be inline and many ui changes.

---
+++ template/list_handler.js
@@ -0,0 +1,6 @@
+$( "#combatants-list" )
+  .sortable( { handle: ".handle" } )
+  .selectable( { handle: ".combatants-list-item", cancel: ".handle" } )
+  .find( "li" )
+      .addClass( "ui-corner-all" )
+      .prepend( "<div class='handle'><span class='ui-icon ui-icon-carat-2-n-s'></span></div>");
